ae3b4af2f8df670a8ee19518eaf05814e33ec700
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-02-16 11:08:03 +0200

    use of graphql metadata - title and author

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,13 @@
 /**
  * Configure your Gatsby site with this file.
- *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
 module.exports = {
+  siteMetadata: {
+    title: 'Full-Stack Bootcamp',
+    author: 'Lauri Kyttälä'
+  },
   plugins: [
     'gatsby-plugin-sass'
   ]
