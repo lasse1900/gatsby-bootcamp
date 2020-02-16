c4f647c51c5cb490de5ffa8eed87a245a57f7345
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-02-16 16:43:15 +0200

    corrected file structure

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,7 +16,7 @@ module.exports.onCreateNode = ({ node, actions }) => {
 
 module.exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions
-    const blogTemplate = path.resolve('./src/pages/templates/blog.js')
+    const blogTemplate = path.resolve('./src/templates/blog.js')
     const res = await graphql(`
         query {
             allMarkdownRemark {
