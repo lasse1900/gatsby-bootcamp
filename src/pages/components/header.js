f8f60ae0445657f96b14211c9b7805e29293267a
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-02-16 13:22:50 +0200

    Markdown posts

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -4,6 +4,7 @@ import { Link, graphql, useStaticQuery } from 'gatsby'
 import headerStyles from './header.module.scss'
 
 const Header = () => {
+  
   const data = useStaticQuery(graphql`
     query {
       site {
