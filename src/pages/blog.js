32459d25904c6b6531b34974c8e715ebb5b56e84
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-02-17 10:45:28 +0200

    changed query to contentful

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,21 +7,24 @@ import blogStyles from './blog.module.scss'
 const BlogPage = () => {
 
   const data = useStaticQuery(graphql`
-    query {
-      allMarkdownRemark {
-        edges {
-          node {
-            frontmatter {
-              title
-              date
-            }
-            fields {
-              slug
-            }
-          }
-        }
+query {
+  allContentfulBlogPost (
+    sort: {
+      fields: publishedDate,
+      order: DESC
+    }
+  ){
+    edges {
+      node {
+        title
+        slug
+        publishedDate(
+          formatString:"MMMM Do YYYY"
+        )
       }
     }
+  }
+}
 `)
 
   return (
@@ -29,12 +32,12 @@ const BlogPage = () => {
       <Layout>
         <h1>My Blog</h1>
         <ol className={blogStyles.posts}>
-          {data.allMarkdownRemark.edges.map((edge) => {
+          {data.allContentfulBlogPost.edges.map((edge) => {
             return (
               <li className={blogStyles.post}>
-                <Link to={`/blog/${edge.node.fields.slug}`}>
-                  <h2>{edge.node.frontmatter.title}</h2>
-                  <p>{edge.node.frontmatter.date}</p>
+                <Link to={`/blog/${edge.node.slug}`}>
+                  <h2>{edge.node.title}</h2>
+                  <p>{edge.node.publishedDate}</p>
                 </Link>
               </li>
             )
