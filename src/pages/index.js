ffbe1f6647297d3bbf589147bb84b75e681af35e
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-02-17 13:07:09 +0200

    removed unnessary link

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => {
       <Head title="Home"/>
       <h1>Hello</h1>
       <h2>I'm Lauri software developer / job seeker living in Vantaa. </h2>
-      <p>Need a developer? <Link to="/contact">Contact me!</Link></p>
     </Layout>
   )
 }
